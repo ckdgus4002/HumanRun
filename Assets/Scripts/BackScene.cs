01104b7fcf5fb3e4c9d0879728afaa2057500084
LCH <ckdgus4002@naver.com>
2019-02-04 23:36:03 +0900

    1.1 commit

--- Assets/Scripts/BackScene.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class BackScene : MonoBehaviour {
-    private void Update() {
-        if(Input.GetButtonDown("Cancel")) SceneManager.LoadScene(0);
-    }
-}
